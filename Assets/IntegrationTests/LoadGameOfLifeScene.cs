1a64971f3f66d53c934571f66baffec57e607d69
Harald Reingruber <harald.reingruber@gmx.at>
2020-01-18 02:02:33 +0100

    Add simple play-mode test

---
+++ Assets/IntegrationTests/LoadGameOfLifeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Zenject;
+
+namespace Tests
+{
+    public class LoadGameOfLifeScene : SceneTestFixture
+    {
+        [UnityTest]
+        public IEnumerator TestGameOfLifeScene()
+        {
+            yield return LoadScene("GameOfLife");
+
+            // Wait a few seconds to ensure the scene starts correctly
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}
